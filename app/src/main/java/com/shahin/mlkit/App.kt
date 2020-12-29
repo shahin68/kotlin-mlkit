bfb4e6fdf35d53c512bb03c63471f327b32aae5a
shahin68 <shahin.montazeri@gmail.com>
2020-12-30 00:37:40 +0330

    ml vision bug workaround

--- app/src/main/java/com/shahin/mlkit/App.kt
+++ app/src/main/java/com/shahin/mlkit/App.kt
@@ -1,12 +1,13 @@
 package com.shahin.mlkit
 
 import android.app.Application
+import androidx.multidex.MultiDexApplication
 import com.shahin.mlkit.di.appModule
 import com.shahin.mlkit.di.repositoryModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
-class App : Application() {
+class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
