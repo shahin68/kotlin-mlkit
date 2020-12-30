54835ee90dc7857dcba13f3d4295b76c63456d95
shahin68 <shahin.montazeri@gmail.com>
2020-12-30 03:31:49 +0330

    initial ui setup

--- app/src/main/java/com/shahin/mlkit/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/shahin/mlkit/ui/fragments/home/HomeFragment.kt
@@ -19,6 +19,10 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.captureView.setOnClickListener {
+
+        }
     }
 
 }
